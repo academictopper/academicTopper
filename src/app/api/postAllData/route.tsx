82147d7b7d523072a729cb1e9104422566ea41f3
Rishabh Jha <rishabhjha0@gmail.com>
2024-10-05 17:10:27 +0530

    include participants in my events

--- src/app/api/postAllData/route.tsx
+++ src/app/api/postAllData/route.tsx
@@ -7,15 +7,24 @@ export async function POST(request:NextRequest) {
     await connect();
     
     const data = await request.json();
-    const { board, class: className, subject, chapter, questionArray, pdfile,isFeatured,exercise,image2 } = data;
+    const { board, class: className, subject, chapter, questionArray, pdfile, isFeatured, exercise, image2 } = data;
 
     if (!board || !className || !subject || !chapter || !questionArray) {
       return NextResponse.json({ error: 'Missing required fields' }, { status: 400 });
     }
-    // Check if a question with the same board, class, subject, and chapter already exists
-    const existingQuestion = await Question.findOne({ board, class: className, subject, chapter,exercise:exercise ? exercise.toString() : null });
+
+    // Condition to include exercise only if the subject is "Mathematics"
+    const query: any = { board, class: className, subject, chapter };
+    
+    if (subject === "Mathematics") {
+      query.exercise = exercise ? exercise : null;
+    }
+
+    // Check if a question with the same board, class, subject, chapter, and exercise (if applicable) already exists
+    const existingQuestion = await Question.findOne(query);
+
     if (existingQuestion) {
-      return NextResponse.json({ error: 'A question with the same board, class, subject, and chapter already exists' }, { status: 409 });
+      return NextResponse.json({ error: 'A question with the same board, class, subject, chapter, and exercise already exists' }, { status: 409 });
     }
 
     const newQuestion = await new Question({
@@ -32,7 +41,7 @@ export async function POST(request:NextRequest) {
 
     await newQuestion.save();
     return NextResponse.json({ message: 'Question saved successfully', question: newQuestion }, { status: 201 });
-  } catch (error:any) {
+  } catch (error: any) {
     return NextResponse.json({ error: 'Error saving question', details: error.message }, { status: 500 });
   }
 }
